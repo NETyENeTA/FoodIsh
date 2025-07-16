891b19d48a297653ddc963b1e9e8d606ebd4a4e4
NETyENeTA <gigasekond2@gmail.com>
2025-07-16 20:46:15 +0500

    [ADDED] Interfaces.

---
+++ Application/Interfaces/ICreateOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodIstAPI.Application.DTOs.Order;
+
+namespace FoodIstAPI.Application.Interfaces;
+
+public interface IOrderService
+{
+    Task<List<CreateOrderDto>> GetAllAsync();
+    Task AddAsync(CreateOrderDto productDto);
+}
