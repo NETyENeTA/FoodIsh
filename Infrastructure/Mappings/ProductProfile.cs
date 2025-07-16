eeb4aa1c8255a031557c8f56cbf75b2815b5eff0
NETyENeTA <gigasekond2@gmail.com>
2025-07-16 20:59:57 +0500

    [ADDED] [CLASS] Mapper's Files (2).

---
+++ Infrastructure/Mappings/ProductProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities;
+using FoodIstAPI.Application.DTOs.Order;
+
+namespace FoodIstAPI.Infrastructure.Mappings;
+
+/// <summary>
+/// [Mapper] for { Product <=> Dto };
+/// </summary>
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+    }
+}
---
+++ Infrastructure/Mappings/UserProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities;
+using FoodIstAPI.Application.DTOs.Order;
+using FoodIstAPI.Application.DTOs.User;
+
+namespace FoodIstAPI.Infrastructure.Mappings;
+
+/// <summary>
+/// [Mapper] for { AddUser <=> Dto, User <=> Dto};
+/// </summary>
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<AddUserDto, User>().ReverseMap();
+        CreateMap<UserDto, User>().ReverseMap();
+    }
+}
