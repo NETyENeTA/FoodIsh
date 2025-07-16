c8ed72cbbdfebb04aead3b81891b551a0d43b10b
NETyENeTA <gigasekond2@gmail.com>
2025-07-16 21:02:17 +0500

    [ADDED] [APP] DB Context.

---
+++ Infrastructure/Data/AppDBContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Data;
+
+/// <summary>
+/// Main Context, can work with User(s), Product(s), Order(s), OrderItem(s).
+/// </summary>
+public class AppDbContext : DbContext
+{
+    // TABLES.
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+    // OPTIONS.
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    // Creating Model.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Order)
+            .WithMany(o => o.Items)
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Product)
+            .WithMany()
+            .HasForeignKey(oi => oi.ProductId);
+    }
+}
